70ca81c84ea34007492727be57e84268d0a412f5
Gabriel <sarais.gabriel@gmail.com>
2022-09-14 15:48:44 +0200

    monitoring scale set

--- terraform/scale-set.tf
+++ terraform/scale-set.tf
@@ -5,7 +5,7 @@ resource "azurerm_subnet" "subnet_app" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_linux_virtual_machine_scale_set" "example" {
+resource "azurerm_linux_virtual_machine_scale_set" "vmss_app" {
   name                = "g2-vmss"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -15,6 +15,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
 
   admin_ssh_key {
     username   = "superman"
+    disable_password_authentication = true
     public_key = azurerm_ssh_public_key.ssh_key.public_key
   }
 
@@ -24,6 +25,9 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
     sku       = "11"
     version   = "latest"
   }
+
+  custom_data = data.cloudinit_config.cloud-init.rendered
+
 
   os_disk {
     storage_account_type = "Premium_LRS"
@@ -38,6 +42,55 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
       name      = "nic_app_config"
       primary   = true
       subnet_id = azurerm_subnet.subnet_app.id
+    }
+  }
+}
+
+resource "azurerm_monitor_autoscale_setting" "mon_auto_scl" {
+  name                = "myAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.vmss_app.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.vmss_app.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+  }
+
+  notification {
+    email {
+      custom_emails                         = ["sarais.gabriel@gmail.com"]
+      custom_emails                         = ["alain.caupin@gmail.com"]
+      custom_emails                         = ["bstewart.ext@simplon.co"]
+      custom_emails                         = ["asawaya.ext@simplon.co"]
+
     }
   }
 }
