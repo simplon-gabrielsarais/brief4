7c5ff548f1fa7e7e538754db5a7e1dd5968940e9
Gabriel <sarais.gabriel@gmail.com>
2022-09-14 10:27:59 +0200

    powershell script

---
+++ powershell/charge.ps1
@@ -0,0 +1,10 @@
+# montée en charge brief 4
+$r = 100
+$i = 0
+while ($i -lt $r)
+{
+    curl -F                                         #dog
+    curl -F                                         #cat
+    $i++
+
+}
