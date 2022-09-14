3bb9f5e2f63fad2b845d3d4b0f89ca956c28cac1
Alfred Sawaya <alfred@huji.fr>
2022-09-14 03:40:42 +0200

    terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,7 +16,7 @@ data "cloudinit_config" "cloud-init" {
 
   part {
     content_type = "text/cloud-config"
-    content      = templatefile("cloud-init.yml", {REDIS_HOST = azurerm_redis_cache.redis.hostname,
-                                                   REDIS_PWD = azurerm_redis_cache.redis.primary_access_key})
+    content      = templatefile("../cloud-init.yml", {REDIS_HOST = azurerm_redis_cache.redis.hostname,
+                                                      REDIS_PWD = azurerm_redis_cache.redis.primary_access_key})
   }
 }
