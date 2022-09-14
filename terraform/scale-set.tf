c0e571b8e8891a6d3110b6ddc9b7c58227c84218
Gabriel <sarais.gabriel@gmail.com>
2022-09-14 14:32:33 +0200

    scalset variables

--- terraform/scale-set.tf
+++ terraform/scale-set.tf
@@ -1,36 +1,43 @@
+resource "azurerm_subnet" "subnet_app" {
+  name                 = "subnet_app"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_linux_virtual_machine_scale_set" "example" {
-  name                = "example-vmss"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                = "g2-vmss"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = "Standard_F2"
   instances           = 1
   admin_username      = "adminuser"
 
   admin_ssh_key {
-    username   = "adminuser"
-    public_key = local.first_public_key
+    username   = "superman"
+    public_key = azurerm_ssh_public_key.ssh_key.public_key
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    publisher = "Debian"
+    offer     = "debian-11"
+    sku       = "11"
     version   = "latest"
   }
 
   os_disk {
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
     caching              = "ReadWrite"
   }
 
   network_interface {
-    name    = "example"
+    name    = "nicss"
     primary = true
 
     ip_configuration {
-      name      = "internal"
+      name      = "nic_app_config"
       primary   = true
-      subnet_id = azurerm_subnet.internal.id
+      subnet_id = azurerm_subnet.subnet_app.id
     }
   }
 }
