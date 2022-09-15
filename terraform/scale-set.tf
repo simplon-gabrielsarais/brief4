aa87903170c8371c778cc468ba8ea15ccc8e98d8
Simplon-AlainCaupin <simplon.alaincaupin@gmail.com>
2022-09-15 09:07:14 +0200

    updated scale set tf file

--- terraform/scale-set.tf
+++ terraform/scale-set.tf
@@ -5,19 +5,32 @@ resource "azurerm_subnet" "subnet_app" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_interface" "nic_app" {
+  name                = "nic_app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "nic_app_config"
+    subnet_id                     = azurerm_subnet.subnet_app.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+
 resource "azurerm_linux_virtual_machine_scale_set" "vmss_app" {
   name                = "g2-vmss"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Standard_F2"
+  sku                 = "Standard_E2d_v4"
   instances           = 1
-  admin_username      = "adminuser"
+  # admin_username      = "adminuser"
 
-  admin_ssh_key {
-    username   = "superman"
-    disable_password_authentication = true
-    public_key = azurerm_ssh_public_key.ssh_key.public_key
-  }
+  # admin_ssh_key {
+  #   username   = "superman"
+  #   public_key = azurerm_ssh_public_key.ssh_key.public_key
+  # }
 
   source_image_reference {
     publisher = "Debian"
@@ -27,15 +40,17 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss_app" {
   }
 
   custom_data = data.cloudinit_config.cloud-init.rendered
-
+  disable_password_authentication = false
+  admin_username = "superman"
+  admin_password = "P@ssw0rd1234!"
 
   os_disk {
-    storage_account_type = "Premium_LRS"
     caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
   network_interface {
-    name    = "nicss"
+    name    = "nic_app"
     primary = true
 
     ip_configuration {
@@ -71,7 +86,7 @@ resource "azurerm_monitor_autoscale_setting" "mon_auto_scl" {
         time_aggregation   = "Average"
         operator           = "GreaterThan"
         threshold          = 70
-        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+        metric_namespace   = "Microsoft.Compute/virtualMachineScaleSets"
       }
 
       scale_action {
@@ -86,11 +101,7 @@ resource "azurerm_monitor_autoscale_setting" "mon_auto_scl" {
 
   notification {
     email {
-      custom_emails                         = ["sarais.gabriel@gmail.com"]
-      custom_emails                         = ["alain.caupin@gmail.com"]
-      custom_emails                         = ["bstewart.ext@simplon.co"]
-      custom_emails                         = ["asawaya.ext@simplon.co"]
-
+      custom_emails                         = ["sarais.gabriel@gmail.com","alain.caupin@gmail.com","bstewart.ext@simplon.co","asawaya.ext@simplon.co"]
     }
   }
 }
